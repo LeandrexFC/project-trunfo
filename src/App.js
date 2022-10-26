b1f2dc2c510ba16c14e30a6d5f6d0c04c2922088
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-10-26 20:21:44 -0300

    Requisito 3 concluído

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardTrunfo: false,
+  };
+
+  onInputChange = (event) => {
+    this.setState({ cardName: event.target.value }, () => {
+      this.setState({
+        cardTrunfo: 'Super Trunfo',
+      });
+    });
+  };
+
   render() {
+    const { cardName, cardTrunfo } = this.state;
+    console.log(cardTrunfo);
     return (
       <div>
         <h1>ADICIONE NOVA CARTA</h1>
         <main>
-          <Form />
+          <Form
+            cardName={ cardName }
+            onInputChange={ this.onInputChange }
+          />
+          <Card />
         </main>
       </div>
     );
