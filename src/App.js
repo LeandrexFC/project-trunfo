83237f01d1901770bdbf07940f3a318e24c33854
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-10-27 18:40:26 -0300

    Requisito 5 concluído

--- src/App.js
+++ src/App.js
@@ -22,6 +22,28 @@ class App extends React.Component {
     });
   };
 
+  validateForm = () => {
+    const { cardName, cardDescription, cardRare, cardImage,
+      cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const cardsName = cardName.length && cardDescription.length
+    && cardRare.length && cardImage.length > 0;
+    const maxValue = 210;
+    const totalSum = parseInt(cardAttr1, 10)
+    + parseInt(cardAttr2, 10)
+    + parseInt(cardAttr3, 10) <= maxValue;
+    const maxAttr = 90;
+    const minAttr = -1;
+    const maxAttr01 = parseInt(cardAttr1, 10) <= maxAttr;
+    const maxAttr02 = parseInt(cardAttr2, 10) <= maxAttr;
+    const maxAttr03 = parseInt(cardAttr3, 10) <= maxAttr;
+    const minAttr01 = parseInt(cardAttr1, 10) > minAttr;
+    const minAttr02 = parseInt(cardAttr2, 10) > minAttr;
+    const minAttr03 = parseInt(cardAttr3, 10) > minAttr;
+
+    return totalSum && cardsName && maxAttr01
+    && maxAttr02 && maxAttr03 && minAttr01 && minAttr02 && minAttr03;
+  };
+
   render() {
     const { cardName, cardDescription, cardImage,
       cardAttr1, cardAttr2, cardAttr3, cardRare, cardTrunfo } = this.state;
@@ -39,6 +61,7 @@ class App extends React.Component {
             cardAttr3={ cardAttr3 }
             cardRare={ cardRare }
             cardTrunfo={ cardTrunfo }
+            isSaveButtonDisabled={ this.validateForm() }
           />
           <Card
             onInputChange={ this.onInputChange }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -103,7 +103,7 @@ class Form extends Component {
           <button
             type="button"
             data-testid="save-button"
-            disabled={ isSaveButtonDisabled }
+            disabled={ !isSaveButtonDisabled }
             onClick={ onSaveButtonClick }
           >
             Salvar
